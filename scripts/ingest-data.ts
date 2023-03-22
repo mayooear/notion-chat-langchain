e93b1f325d12c3cd890f7add80142c3c4e4052d1
Memento Mori <xingfanxia@gmail.com>
2023-03-22 00:30:43 -0700

    Fix outdated dependencies

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,12 +1,18 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { PineconeStore } from 'langchain/vectorstores';
+import { PineconeLibArgs } from 'langchain/vectorstores/pinecone.js';
 import { pinecone } from '@/utils/pinecone-client';
 import { processMarkDownFiles } from '@/utils/helpers';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-
+import type { VectorOperationsApi } from "@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch";
 /* Name of directory to retrieve files from. You can change this as required */
 const directoryPath = 'Notion_DB';
+interface PineconeLibArgs {
+    pineconeIndex: VectorOperationsApi;
+    textKey?: string;
+    namespace?: string;
+}
 
 export const run = async () => {
   try {
@@ -20,19 +26,30 @@ export const run = async () => {
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
+    // console.log('split docs', docs);
+    console.log('split docs ok')
+    console.log('docs length', docs.length)
+    for (let i = 0; i < docs.length; i += 100) {
+        const slice = docs.slice(i, i + 100);
+        console.log('creating vector store...');
+        /*create and store the embeddings in the vectorStore*/
+        const embeddings = new OpenAIEmbeddings();
+        console.log('initiated embeddings');
+        const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+        console.log('initiated index');
+        let dbConfig:PineconeLibArgs = {
+            pineconeIndex: pinecone.Index(PINECONE_INDEX_NAME),
+            namespace: PINECONE_NAME_SPACE,
+        };
+        await PineconeStore.fromDocuments(
+          slice,
+          embeddings,
+          dbConfig
+        );
+        // Do something with the slice
+      }
 
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
-    await PineconeStore.fromDocuments(
-      index,
-      docs,
-      embeddings,
-      'text',
-      PINECONE_NAME_SPACE, //optional namespace for your vectors
-    );
+
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
